b294b163bfd015168dd20613b2cea00ec87c8e85
Valentin Carlesso <valentin.carlesso@universo.univates.br>
2026-09-02 16:15:49 -0300

    feat: ajustes de texto

--- src/main/java/com/mycompany/apicativo/Apicativo.java
+++ src/main/java/com/mycompany/apicativo/Apicativo.java
@@ -7,13 +7,12 @@ import javax.swing.SwingUtilities;
 public class Apicativo {
 
     public static void main(String[] args) {
-        // Inicializa o tema FlatLaf
         FlatDarkLaf.setup();
 
         SwingUtilities.invokeLater(() -> {
             JOptionPane.showMessageDialog(
                 null, 
-                "Sistema ApiCativo iniciado com sucesso!", 
+                "Sistema ApiCativo", 
                 "ApiCativo - Controle Apícola", 
                 JOptionPane.INFORMATION_MESSAGE
             );

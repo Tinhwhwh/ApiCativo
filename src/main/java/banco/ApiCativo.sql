2f9fc53128b01d147fcdb82321ad85037bf07c40
Valentin Carlesso <valentin.carlesso@universo.univates.br>
2026-09-23 12:41:18 -0300

    refactor: corrige estrutura de pastas e remove pasta banco nao utilizada

--- src/main/java/banco/ApiCativo.sql
+++
@@ -1,9 +0,0 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  valentin.carlesso
- * Created: 2 de set. de 2026
- */
-
